d41424dc73ccbaa1b255d61b60bd292d2059b35c
Enoch2k2 <enoch2k2@hotmail.com>
2024-06-04 10:40:20 -0500

    updated state and events for lecture

--- phase-2/week-1/day-2/state-and-events/src/App.jsx
+++ phase-2/week-1/day-2/state-and-events/src/App.jsx
@@ -1,27 +1,10 @@
-import BlueParagraph from './StyledComponents/BlueParagraph'
 import Header from './components/Header'
-import { baseUrl, headers } from './globals'
-import { useEffect } from 'react'
 
 function App() {
-  
-  useEffect(() => {
-    console.log('a')
-  }, [])
   
   return (
     <div>
       <Header name="Bob" age={32} />
-      <Header name="Sam" age={23} />
-      <BlueParagraph>
-        Here's a p tag!
-      </BlueParagraph>
-      <BlueParagraph>
-        Here's another p tag!
-      </BlueParagraph>
-      <BlueParagraph>
-        Here's another blue p tag!
-      </BlueParagraph>
     </div>
   )
 }
--- phase-2/week-1/day-2/state-and-events/src/components/Header.jsx
+++ phase-2/week-1/day-2/state-and-events/src/components/Header.jsx
@@ -1,2 +1,6 @@
 
-export default ({ name, age }) => <h1>Hello { name }! You are { age + 1 } years old!</h1>
+const Header = ({ name, age }) => {
+  return <h1>Hello { name }! You are { age + 1 } years old!</h1>
+}
+
+export default Header
