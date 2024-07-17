0bf5818449d0cc4f32657a4946fc079cacdaf1a5
Enoch2k2 <enoch2k2@hotmail.com>
2024-07-17 14:11:34 -0500

    finished p4 w1 d3 demo

---
+++ phase-4/week-1/day-3/patch-and-delete-with-error-handeling/server/config.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.schema import MetaData
+from flask_restful import Api
+
+naming_convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+metaData = MetaData(naming_convention=naming_convention)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app=app, metadata=metaData)
+
+api = Api(app=app)
+
+migrate = Migrate(app=app, db=db, render_as_batch=True)
